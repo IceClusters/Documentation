589f1036659b33cab468361c0d909a9a918378ab
zKXDEX <deiividdlk@gmail.com>
2023-10-26 14:50:17 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withNextra = require('nextra')({
 module.exports = {
   ...withNextra(),
   rewrites: null,
+  basePath: '/documentation',
   output: 'export',
   images: {
     unoptimized: true,
