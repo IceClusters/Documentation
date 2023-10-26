1f8381f0fa44b1f86d0f25c0b1ff592e275d087d
zKXDEX <deiividdlk@gmail.com>
2023-10-26 11:00:54 +0200

    .

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://iceclusters.github.io/Documentation/',
+  siteUrl: 'https://iceclusters.github.io/',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   outDir: './out',
