a20cd532f517fc6ec87bcdfc7fc86b30bcce18b0
zKXDEX <deiividdlk@gmail.com>
2023-10-27 11:46:31 +0200

    .

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ global.Headers = global.Headers || require('node-fetch').Headers;
 
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx'
+    themeConfig: './theme.config.tsx'
   })
    
   module.exports = withNextra()
