92ec6fea4b85641b20db17bb1fa84baf8341ec0b
zKXDEX <deiividdlk@gmail.com>
2023-10-28 13:23:42 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const isProduction = process.env.NODE_ENV === "production";
 const assetPrefix = isProduction ? "/introduction" : "";
 module.exports = {
 	...withNextra(),
-	assetPrefix,
 	basePath: "/docs",
 	images: {
 		unoptimized: true,
