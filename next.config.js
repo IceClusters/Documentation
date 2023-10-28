93dea927586b6971d31f3ca339953b7fa1ca1c85
zKXDEX <deiividdlk@gmail.com>
2023-10-28 13:15:30 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,6 +13,3 @@ module.exports = {
 		unoptimized: true,
 	},
 };
-
-// If you have other Next.js configurations, you can pass them as the parameter:
-// module.exports = withNextra({ /* other next.js config */ })
