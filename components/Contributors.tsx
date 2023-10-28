12e2348996c66476a2cdaa430e3346aeda75e323
zKXDEX <deiividdlk@gmail.com>
2023-10-28 13:01:23 +0200

    prefix

--- components/Contributors.tsx
+++ components/Contributors.tsx
@@ -6,30 +6,30 @@ const Contributors: React.FC<{ image: string; name: string; gitUser: string }> =
     const blacklistedNames = ["DaniGP17", "zKXDEX"]
     const [contributors, setContributors] = useState([]);
 
-    // useEffect(() => {
-    //     fetch("https://api.github.com/repos/IceClusters/icmysql/contributors?anon=1")
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //             setContributors(function(){
-    //                 let retval = [];
-    //                 for(let i = 0; i < data.length; i++){
-    //                     if(blacklistedNames.includes(data[i].login)) continue;
-    //                     console.log(data[i].login)
-    //                     retval.push({
-    //                         name: data[i].login,
-    //                         avatar: data[i].avatar_url,
-    //                         commits: data[i].contributions
-    //                     })
-    //                 }
-    //                 return retval;
-    //             });
-    //         })
-    //         .catch((error) => console.log("Error: ", error));
-    // }, []);
+    useEffect(() => {
+        fetch("https://api.github.com/repos/IceClusters/icmysql/contributors?anon=1")
+            .then((response) => response.json())
+            .then((data) => {
+                setContributors(function(){
+                    let retval = [];
+                    for(let i = 0; i < data.length; i++){
+                        if(blacklistedNames.includes(data[i].login)) continue;
+                        console.log(data[i].login)
+                        retval.push({
+                            name: data[i].login,
+                            avatar: data[i].avatar_url,
+                            commits: data[i].contributions
+                        })
+                    }
+                    return retval;
+                });
+            })
+            .catch((error) => console.log("Error: ", error));
+    }, []);
     
     return (
     <div className="container__contributor">
-        {/* {contributors.map((contributor, i) => (
+        {contributors.map((contributor, i) => (
             <div className="card--c">
                 <img className='avatar' src={contributor.avatar}/>
                 <div className="space__info">
@@ -37,7 +37,7 @@ const Contributors: React.FC<{ image: string; name: string; gitUser: string }> =
                     <h5 className='desc__c'>{contributor.commits} commits</h5>
                 </div>
             </div>
-        ))} */}
+        ))}
         
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -2,14 +2,16 @@ const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.tsx',
   })
-   
-  module.exports = {
-    ...withNextra(),
-    basePath: "/docs",
-    images: {
-      unoptimized: true,
-    },
-  };
+  const isProduction = process.env.NODE_ENV === "production";
+  const assetPrefix = isProduction ? "/introduction" : ""; 
+module.exports = {
+  ...withNextra(),
+  assetPrefix,
+  basePath: "/docs/" + assetPrefix,
+  images: {
+    unoptimized: true,
+  },
+};
    
   // If you have other Next.js configurations, you can pass them as the parameter:
   // module.exports = withNextra({ /* other next.js config */ })
