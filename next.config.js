7c777e23fd731f16bb977b0a6a9d49d168da2b37
zKXDEX <deiividdlk@gmail.com>
2023-10-28 13:01:34 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.tsx',
-  })
-  const isProduction = process.env.NODE_ENV === "production";
-  const assetPrefix = isProduction ? "/introduction" : ""; 
+	theme: 'nextra-theme-docs',
+	themeConfig: './theme.config.tsx',
+})
+
+const isProduction = process.env.NODE_ENV === "production";
+const assetPrefix = isProduction ? "/introduction" : "";
 module.exports = {
-  ...withNextra(),
-  assetPrefix,
-  basePath: "/docs/" + assetPrefix,
-  images: {
-    unoptimized: true,
-  },
+	...withNextra(),
+	assetPrefix,
+	basePath: "/docs/" + assetPrefix,
+	images: {
+		unoptimized: true,
+	},
 };
-   
-  // If you have other Next.js configurations, you can pass them as the parameter:
-  // module.exports = withNextra({ /* other next.js config */ })
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
